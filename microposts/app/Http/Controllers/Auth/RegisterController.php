7b11def2da13542f9fc829a65f5cdaca7631e62b
taro.kirameki <ubuntu@ip-172-31-22-37.ap-northeast-1.compute.internal>
2022-06-08 07:54:28 +0000

    user registration

--- microposts/app/Http/Controllers/Auth/RegisterController.php
+++ microposts/app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,8 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
 class RegisterController extends Controller
 {
