d09499eb3a6bc7697873df2b4328a5ce763cb0df
taro.kirameki <ubuntu@ip-172-31-22-37.ap-northeast-1.compute.internal>
2022-06-09 04:34:16 +0000

    usere pages

---
+++ microposts/resources/views/users/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+    {{-- ユーザ一覧 --}}
+    @include('users.users')
+@endsection
+
+
+@if (count($users) > 0)
+    <ul class="list-unstyled">
+        @foreach ($users as $user)
+            <li class="media">
+                {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
+                <img class="mr-2 rounded" src="{{ Gravatar::get($user->email, ['size' => 50]) }}" alt="">
+                <div class="media-body">
+                    <div>
+                        {{ $user->name }}
+                    </div>
+                    <div>
+                        {{-- ユーザ詳細ページへのリンク --}}
+                        <p>{!! link_to_route('users.show', 'View profile', ['user' => $user->id]) !!}</p>
+                    </div>
+                </div>
+            </li>
+      @endforeach
+            <ul class="navbar-nav">
+                @if (Auth::check())
+                    {{-- ユーザ一覧ページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('users.index', 'Users', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item dropdown">
+    {{ $users->links() }}
+@endif
