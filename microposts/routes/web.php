0a11439f92ed6e48e6ed80b7e20c192d141eb91e
taro.kirameki <ubuntu@ip-172-31-22-37.ap-northeast-1.compute.internal>
2022-06-09 01:47:10 +0000

    user login

--- microposts/routes/web.php
+++ microposts/routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
