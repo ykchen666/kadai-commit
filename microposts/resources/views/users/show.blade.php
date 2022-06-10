be8730cfe00602076b6853ce6bfa51d04deeddc2
taro.kirameki <ubuntu@ip-172-31-22-37.ap-northeast-1.compute.internal>
2022-06-10 02:54:40 +0000

    follow, unfollow

--- microposts/resources/views/users/show.blade.php
+++ microposts/resources/views/users/show.blade.php
@@ -13,6 +13,20 @@
                 </div>
             </div>
         </aside>
+                <aside class="col-sm-4">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">{{ $user->name }}</h3>
+                </div>
+                <div class="card-body">
+                    {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
+                    <img class="rounded img-fluid" src="{{ Gravatar::get($user->email, ['size' => 500]) }}" alt="">
+                </div>
+            </div>
+            {{-- フォロー／アンフォローボタン --}}
+            @include('user_follow.follow_button')
+        </aside>
+
         <div class="col-sm-8">
             <ul class="nav nav-tabs nav-justified mb-3">
                 {{-- ユーザ詳細タブ --}}
@@ -22,6 +36,27 @@
                 {{-- フォロワー一覧タブ --}}
                 <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
             </ul>
+        </div>
+    </div>
+@endsection
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <aside class="col-sm-4">
+            {{-- ユーザ情報 --}}
+            @include('users.card')
+        </aside>
+        <div class="col-sm-8">
+            {{-- タブ --}}
+            @include('users.navtabs')
+            @if (Auth::id() == $user->id)
+                {{-- 投稿フォーム --}}
+                @include('microposts.form')
+            @endif
+            {{-- 投稿一覧 --}}
+            @include('microposts.microposts')
         </div>
     </div>
 @endsection
